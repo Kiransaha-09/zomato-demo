13f830898379763193fa2d50aed1efae87e2190e
Kiransaha-09 <ksaha3508@gmail.com>
2025-04-04 08:51:21 +0530

    createdcontext

--- src/Components/CartButton/CartButton.jsx
+++ src/Components/CartButton/CartButton.jsx
@@ -1,18 +1,13 @@
-import React from "react";
+import React,{useContext} from "react";
+import { ProductContext } from "../../Context/ProductContext";
+import CartWidjet from "../CartWidjet/CartWidjet";
 
 export default function CartButton() {
   const {
-    products,
     cartItems,
-    setCartItem,
-    isLoading,
-    totalItems,
-    setItemsPerPage,
-    itemsPerPage,
-    searchText,
-    setSearchText,
-    searchProduct,
-    fetchCards,
+    setCartItem 
   } = useContext(ProductContext);
-  return <h1>Hello cart</h1>;
+  return(
+     <CartWidjet cartItems={cartItems} setCartItem={setCartItem} />
+  )
 }
